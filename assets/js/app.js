1bd48552b83aa68e902b3b39a3b8e03c77cf1e03
sanjuverma373 <sv0240866@gmail.com>
2023-11-08 16:53:36 +0530

    section modified

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,8 +8,7 @@ let home = document.querySelector(".home");
 let about = document.querySelector(".about");
 let services = document.querySelector(".services");
 let pricing = document.querySelector(".pricing");
-let faq = document.querySelector(".faq");
-let login = document.querySelector(".faq");
+
 
 menuIcon.addEventListener("click", function () {
     if (a.classList !== document.querySelector("fix-lft")) {
@@ -49,11 +48,27 @@ pricing.addEventListener("click", function () {
     cross5.classList.toggle("cross-2");
     cross6.classList.toggle("cross-3");
 });
-faq.addEventListener("click", function () {
-    a.classList.remove("fix-lft");
-    flow.classList.remove("flow-hidden");
-    cross4.classList.toggle("cross-1");
-    cross5.classList.toggle("cross-2");
-    cross6.classList.toggle("cross-3");
+
+
+
+let backToTop = document.getElementById('backtotop')
+backToTop.addEventListener("click", function () {
+    window.scrollTo(0, 0)
+});
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 500) {
+        backToTop.classList.remove('display_none');
+        backToTop.classList.add('display_block');
+    } else {
+        backToTop.classList.remove('display_block');
+        backToTop.classList.add('display_none');
+    }
 });
 
+// priloder
+const preloader = document.getElementById("preloader")
+setTimeout(() => {
+    document.getElementById("preloader").classList.add("d-none")
+    document.body.classList.remove("flow-hidden")
+
+}, 2000)
